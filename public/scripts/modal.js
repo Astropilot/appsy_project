658b6b88b5ed55fcb6c69c4055e2e1bcd2e5fe95
Astropilot <mimichackeur@gmail.com>
2019-11-15 09:54:55 +0100

    Mise en plugin JQuery du système de modals

---
+++ public/scripts/modal.js
@@ -0,0 +1,22 @@
+(function( $ ) {
+
+  $.fn.showModal = function() {
+    var that = this;
+    $(that).find('.close').on('click.close', function () {
+      that.removeClass('modal-active');
+      $(this).off('click.close');
+      $(window).off('click.window');
+    });
+
+    $(window).on('click.window', function (event) {
+      if (event.target == that.get(0)) {
+        that.removeClass('modal-active');
+        $(this).off('click.window');
+        $(that, '.close').off('click.close');
+      }
+    })
+
+    return that.addClass('modal-active');
+  };
+
+}( jQuery ));
