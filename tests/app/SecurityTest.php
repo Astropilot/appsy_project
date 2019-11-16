e96dbce49259246c9d10573be15d36eda32848b5
Yohann MARTIN <mimichackeur@gmail.com>
2019-11-16 23:32:56 +0100

    Correction du test unitaire sur la fonction hashPass

--- tests/app/SecurityTest.php
+++ tests/app/SecurityTest.php
@@ -10,11 +10,11 @@ class SecurityTest extends TestCase {
     public function testHashPass() {
         $this->assertSame(
             '4e569b0569294ed18f8abd8d192dac45ed2e1c7bfb3c6bd54adecfa31385baaa',
-            Security::hashPass('Foo')
+            Security::hashPass('Foo', 'a3t=Xc7G?xyUR!YP')
         );
         $this->assertSame(
             '98f820c51b1096a3ad9d0196932cee41fe4c06574a88a0fd3557c3992e22b77a',
-            Security::hashPass('Bar')
+            Security::hashPass('Bar', 'a3t=Xc7G?xyUR!YP')
         );
     }
 
